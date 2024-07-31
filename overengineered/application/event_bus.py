0d96a2b21f2634adb653d1c331aa90cda168e608
Sebastian Beyer <sebastian.beyer@live.com>
2024-07-31 21:54:13 +0200

    Fix event bus

--- overengineered/application/event_bus.py
+++ overengineered/application/event_bus.py
@@ -1,18 +1,20 @@
-from typing import Dict, List, Callable
 import asyncio
+from typing import Dict, List, Callable, Any, Coroutine, Union
 
 
 class EventBus:
     def __init__(self):
-        self.subscribers: Dict[str, List[Callable]] = {}
+        self.subscribers: Dict[str, List[Union[Callable, Coroutine]]] = {}
 
-    def subscribe(self, event_type: str, callback: Callable) -> None:
+    def subscribe(self, event_type: str, callback: Union[Callable, Coroutine]) -> None:
         if event_type not in self.subscribers:
             self.subscribers[event_type] = []
         self.subscribers[event_type].append(callback)
 
-    async def publish(self, event_type: str, data=None) -> None:
+    async def publish(self, event_type: str, data: Any = None) -> None:
         if event_type in self.subscribers:
-            await asyncio.gather(
-                *[callback(data) for callback in self.subscribers[event_type]]
-            )
+            for callback in self.subscribers[event_type]:
+                if asyncio.iscoroutinefunction(callback):
+                    await callback(data)
+                else:
+                    callback(data)
